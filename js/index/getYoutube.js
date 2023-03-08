3e50e15d77518c8f8026dc0440693a465e3b0566
zenghyun <pipo36@naver.com>
2023-03-08 15:52:55 +0900

    first complete

--- js/index/getYoutube.js
+++ js/index/getYoutube.js
@@ -1,40 +1,40 @@
 
-// function getYoutube(data) {
-//     const $videoLists = document.querySelector('.youtube-container');
-//     const $videoTemplate = document.querySelector('.youtube-iframe-template');
-//     const $weatheriframe = document.importNode($videoTemplate.content, true);
-//     const videoId = data.items[0].id.videoId
+function getYoutube(data) {
+    const $videoLists = document.querySelector('.youtube-container');
+    const $videoTemplate = document.querySelector('.youtube-iframe-template');
+    const $weatheriframe = document.importNode($videoTemplate.content, true);
+    const videoId = data.items[0].id.videoId
 
-//     let link = data.items[0].snippet.thumbnails.high.url;
-//     $weatheriframe.querySelector('.youtube-link').setAttribute('href', `https://www.youtube.com/watch?v=${videoId}`);
-//     $weatheriframe.querySelector('.thumbnail').style.backgroundImage = `url(${link})`;
-//     $weatheriframe.querySelector('.thumbnail-title').insertAdjacentHTML('afterbegin', `${data.items[0].snippet.title}`);
+    let link = data.items[0].snippet.thumbnails.high.url;
+    $weatheriframe.querySelector('.youtube-link').setAttribute('href', `https://www.youtube.com/watch?v=${videoId}`);
+    $weatheriframe.querySelector('.thumbnail').style.backgroundImage = `url(${link})`;
+    $weatheriframe.querySelector('.thumbnail-title').insertAdjacentHTML('afterbegin', `${data.items[0].snippet.title}`);
 
-//     $videoLists.append($weatheriframe);
-// }
-// (function Youtube() {
+    $videoLists.append($weatheriframe);
+}
+(function Youtube() {
 
-//     const YOUTUBE_API = "AIzaSyAbeeym1EwOe9RvjseMaOZB5h-7RYcS4l0";
-//     const YOUTUBE_CHANNEL = "UCs1omgoHHPENxs4b-fwMpPQ";
+    const YOUTUBE_API = "AIzaSyAbeeym1EwOe9RvjseMaOZB5h-7RYcS4l0";
+    const YOUTUBE_CHANNEL = "UCs1omgoHHPENxs4b-fwMpPQ";
 
-//     const url = `https://www.googleapis.com/youtube/v3/search?list=PLsU45F2D-sEanSDUJ0LUye7uK8A66zDiL&part=snippet&maxResults=1&channelId=${YOUTUBE_CHANNEL}&type=video&order=date&key=${YOUTUBE_API}`;
+    const url = `https://www.googleapis.com/youtube/v3/search?list=PLsU45F2D-sEanSDUJ0LUye7uK8A66zDiL&part=snippet&maxResults=1&channelId=${YOUTUBE_CHANNEL}&type=video&order=date&key=${YOUTUBE_API}`;
     
-//     fetch(url)
-//         .then(response => {
-//             if (response.status >= 200 && response.status < 300) {
-//                 return response.json();
-//             } else {
-//                 return response.json().then(errData => {
-//                     console.log(errData);
-//                     throw new Error('Something went wrong');
-//                 });
-//             }
-//         })
-//         .catch(error => {
-//             console.log(error);
-//             throw new Error('Something went wrong.');
-//         })
-//         .then((data) => {
-//             getYoutube(data);
-//         })
-// })();
+    fetch(url)
+        .then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return response.json();
+            } else {
+                return response.json().then(errData => {
+                    console.log(errData);
+                    throw new Error('Something went wrong');
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            throw new Error('Something went wrong.');
+        })
+        .then((data) => {
+            getYoutube(data);
+        })
+})();
