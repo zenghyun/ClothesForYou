a215d14f50ace44eaf140ad9e49bcb06ff06e3bf
zenghyun <pipo36@naver.com>
2023-06-29 15:12:08 +0900

    Update Project Setting

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var TerserWebpackPlugin = require("terser-webpack-plugin"); // TerserWebpackPlugin import
+
+module.exports = {
+  mode: "none",
+  entry: {
+    main: "./js/index/app.js",
+    additional: [
+      "./js/index/choiceTip.js",
+      "./js/index/clothesSlider.js",
+      "./js/index/getYoutube.js",
+      "./js/index/scroll.js",
+    ],
+    myClothes: [
+        "./js/myClothes/app.js",
+        "./js/myClothes/colorSet.js",
+        "./js/myClothes/scroll.js",
+        "./js/myClothes/uploadImage.js",
+      ],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|pages)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "./images",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserWebpackPlugin()], // TerserWebpackPlugin 사용
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./index.html", 
+      excludeChunks: ["myClothes"], // myClothes.js를 index.html에 적용하지 않음
+    }),
+    new HtmlWebpackPlugin({
+      filename: "views/myClothes.html",
+      template: "./views/myClothes.html",
+      chunks: ["myClothes"], // myClothes.js와 연결
+    }),
+    new webpack.ProgressPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "css", to: "css" },
+        { from: "scss", to: "scss" },
+        { from: "images", to: "images"},
+        { from: "images", to: "images"},
+        { from: "font", to: "font"},
+      ],
+    }),
+  ],
+};
